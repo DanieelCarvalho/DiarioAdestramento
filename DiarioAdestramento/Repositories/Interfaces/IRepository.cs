2e76d5b2d21ef206d0401d270c3dc8840020b20a
Daniel Carvalho <danielscg2012@gmail.com>
2026-07-09 11:29:32 -0300

    feat: adiciona IRepository e implementação Repository base
    - Cria interface IRepository com operações CRUD
    - Implementa Repository como classe base para repositórios
    - Estrutura inicial para camada de dados do DiarioAdestramento

---
+++ DiarioAdestramento/Repositories/Interfaces/IRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq.Expressions;
+
+namespace DiarioAdestramento.Repositories.Interfaces;
+
+public interface  IRepository<T>
+{
+    Task<IEnumerable<T>> GetAllAsync();
+    Task<T?> GetAsync(Expression<Func<T, bool>> predicate);
+    Task<T> AddAsync(T entity);
+    Task<T> UpdateAsync(T entity);
+    Task<T> DeleteAsync(T entity);
+
+
+}
---
+++ DiarioAdestramento/Repositories/Repository.cs
@@ -0,0 +1,52 @@
+﻿using DiarioAdestramento.Context;
+using DiarioAdestramento.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace DiarioAdestramento.Repositories;
+
+public class Repository<T> : IRepository<T> where T : class
+{
+    private readonly AppDbContext _context;
+
+    public Repository(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<T> AddAsync(T entity)
+    {
+        _context.Set<T>().Add(entity);
+        await _context.SaveChangesAsync();
+        return entity;
+    }
+
+    public async Task<T> DeleteAsync(T entity)
+    {
+       _context.Set<T>().Remove(entity);
+        await _context.SaveChangesAsync();
+        return entity;
+    }
+
+    public async Task<IEnumerable<T>> GetAllAsync()
+    {
+       return await _context.Set<T>()
+         .AsNoTracking()
+         .ToListAsync();
+       
+    }
+
+    public async Task<T?> GetAsync(Expression<Func<T, bool>> predicate)
+    {
+        return await _context.Set<T>()
+         .AsNoTracking()
+         .FirstOrDefaultAsync(predicate);
+    }
+
+    public async Task<T> UpdateAsync(T entity)
+    {
+       _context.Set<T>().Update(entity);
+       await _context.SaveChangesAsync();
+        return entity;
+    }
+}
