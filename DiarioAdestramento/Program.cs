4c016fad2001f96f0313023a1f2699104f1eb5cf
Daniel Carvalho <danielscg2012@gmail.com>
2026-07-20 11:42:04 -0300

    refactor: mover ExceptionMiddleware e limpar Program.cs

--- DiarioAdestramento/Program.cs
+++ DiarioAdestramento/Program.cs
@@ -1,49 +1,22 @@
-using DiarioAdestramento.Context;
-using DiarioAdestramento.Extensions;
-using DiarioAdestramento.Repositories;
-using DiarioAdestramento.Repositories.Interfaces;
-using DiarioAdestramento.Services;
-using Microsoft.EntityFrameworkCore;
+using DiarioAdestramento.Extensions.ApplicationBuilderExtensions;
+using DiarioAdestramento.Extensions.AppServicesExtensions;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.AddApiSwagger()          
+       .AddPersistence()         
+       .AddRepositories()
+       .AddExternalServices()
+       .AddControllers();       
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-builder.Services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
-builder.Services.AddScoped<ILocalRepository, LocalRepository>();
-builder.Services.AddScoped<IRegistroClimaRepository, RegistroClimaRepository>();
-builder.Services.AddScoped<ISessaoTreinoRepository, SessaoTreinoRepository>();
-builder.Services.AddScoped<ICachorroRepository, CachorroRepository>();
-
-builder.Services.AddHttpClient<IClimaService, OpenMeteoClimaService>(client =>
-{
-    client.BaseAddress = new Uri("https://archive-api.open-meteo.com/");
-    client.Timeout = TimeSpan.FromSeconds(10);
-});
-
-var conectionString = builder.Configuration.GetConnectionString("SqliteConnectionString");
-
-
-builder.Services.AddDbContext<AppDbContext>(options =>
-      options.UseSqlite(conectionString));
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-    app.UseDeveloperExceptionPage();
-}
-else
-{
-    app.ConfigureExceptionHandler();
-}
+app.UseExceptionHandling(app.Environment)
+    .UseSwaggerMiddleware()
+    .UseAppCors();
+
 
 app.UseHttpsRedirection();
 
